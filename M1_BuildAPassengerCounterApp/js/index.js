bcf7f6720f7795d260463643153170ed713ea8a6
erickariasec <erickarias1999@hotmail.com>
2022-06-09 19:00:00 -0500

    Scrimba, Learn JavaScript, Build a Passenger Counter App, C004 - Write Your First JavaScript Variable

--- M1_BuildAPassengerCounterApp/js/index.js
+++ M1_BuildAPassengerCounterApp/js/index.js
@@ -1,1 +1,13 @@
-document.getElementById("count-elements").innerText = 5;
+// document.getElementById("count-elements").innerText = 5;
+
+let count = 0;
+
+console.log(count);
+
+// 1. Create a variable, myAge, and set its value to your age
+
+let myAge = 23;
+
+// 2. Log the myAge variable to the console
+
+console.log(myAge);
