34c52f4107acb626c088ca1f8a8ba0a8c0925073
erickariasec <erickarias1999@hotmail.com>
2022-06-17 18:37:13 -0500

    Scrimba, Learn JavaScript, Build a Passenger Counter App, C018 - Create the Save Button

--- M1_BuildAPassengerCounterApp/js/index.js
+++ M1_BuildAPassengerCounterApp/js/index.js
@@ -1,9 +1,13 @@
-// grab the count-elements element, store it in a countElements variable
 let countElements = document.getElementById("count-elements");
 let count = 0;
 
 function increment() {
     count = count + 1;
-    // set countElements's innerText to the count
     countElements.innerText = count;
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save() {
+    console.log(count);
 }
