9e29d6c238395165b29578fbfcd319de1000df4e
erickariasec <erickarias1999@hotmail.com>
2022-06-17 18:19:42 -0500

    Scrimba, Learn JavaScript, Build a Passenger Counter App, C016 - The Document Object Model (explanation)

--- M1_BuildAPassengerCounterApp/js/index.js
+++ M1_BuildAPassengerCounterApp/js/index.js
@@ -1,18 +1,10 @@
-// document.getElementById("count-elements").innerText = 5;
-
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
-
-// camelCase
 let countElements = document.getElementById("count-elements");
-
-console.log(countElements);
-
 let count = 0;
 
 function increment() {
     count = count + 1;
     countElements.innerText = count;
 }
+
+// The DOM 
+// How you use JavaScript to modify a website.
