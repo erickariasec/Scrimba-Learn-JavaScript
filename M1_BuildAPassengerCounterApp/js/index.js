de23994b4654f0836dd38df479892d9bdb5088e9
erickariasec <erickarias1999@hotmail.com>
2022-06-17 18:23:57 -0500

    Scrimba, Learn JavaScript, Build a Passenger Counter App, C017 - Display the Count with innerText

--- M1_BuildAPassengerCounterApp/js/index.js
+++ M1_BuildAPassengerCounterApp/js/index.js
@@ -1,10 +1,9 @@
+// grab the count-elements element, store it in a countElements variable
 let countElements = document.getElementById("count-elements");
 let count = 0;
 
 function increment() {
     count = count + 1;
+    // set countElements's innerText to the count
     countElements.innerText = count;
 }
-
-// The DOM 
-// How you use JavaScript to modify a website.
