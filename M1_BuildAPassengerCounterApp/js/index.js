a2905bf490c0b2089852b7d035b6b737438ac802
erickariasec <erickarias1999@hotmail.com>
2022-06-17 17:22:59 -0500

    Scrimba, Learn JavaScript, Build a Passenger Counter App, C013 - Write a Function that Increments

--- M1_BuildAPassengerCounterApp/js/index.js
+++ M1_BuildAPassengerCounterApp/js/index.js
@@ -16,16 +16,19 @@
 
 
 
-// Write a Function that Logs the Sum
+// Write a Function that Increments
 
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
+let lapsCompleted = 0;
 
-// Create a function that logs out the sum of all the lap times
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
 
-function logOutSumLapTimes() {
-    console.log(lap1 + lap2 + lap3);
+function incrementLapsCompleted() {
+    lapsCompleted = lapsCompleted + 1;
 }
 
-logOutSumLapTimes();
+incrementLapsCompleted();
+incrementLapsCompleted();
+incrementLapsCompleted();
+
+console.log(lapsCompleted);
