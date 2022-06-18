17d06a2969b1f682b95792ed9c9436687ba82f8a
erickariasec <erickarias1999@hotmail.com>
2022-06-18 17:19:01 -0500

    Scrimba, Learn JavaScript, Build a Passenger Counter App, C025 - Use plus equal for count

--- M1_BuildAPassengerCounterApp/js/index.js
+++ M1_BuildAPassengerCounterApp/js/index.js
@@ -1,44 +1,12 @@
-// let countElements = document.getElementById("count-elements");
+let countElements = document.getElementById("count-elements");
 let count = 0;
 
 function increment() {
-    count = count + 1;
+    // Change this to use the plus equal technique you've learned
+    count += 1;
     countElements.innerText = count;
 }
 
 function save() {
     console.log(count);
 }
-
-
-
-// Improve the Message with String Concatenation
-
-
-// HTML
-
-// <html>
-//     <head>
-//     </head>
-//     <body>
-//         <p id="welcome-el"></p>
-//         <script src="index.js"></script>
-//     </body>
-// </html>;
-
-
-// JAVASCRIPT
-
-
-// let welcomeEl = document.getElementById("welcome-el")
-
-// let name = "Per Harald Borgen"
-// let greeting = "Welcome back "
-
-// welcomeEl.innerText = greeting + name
-
-// Add an emoji to the end! 👋
-// WRITE YOUR CODE BELOW HERE
-// HINT: count = count + 1
-
-// welcomeEl.innerText += " 👋";
