6819f02df5223ca868ab1825b7d09c651a814572
erickariasec <erickarias1999@hotmail.com>
2022-06-18 16:39:08 -0500

    Scrimba, Learn JavaScript, Build a Passenger Counter App, C020 - Write Your First String Variable

--- M1_BuildAPassengerCounterApp/js/index.js
+++ M1_BuildAPassengerCounterApp/js/index.js
@@ -9,3 +9,15 @@ function increment() {
 function save() {
     console.log(count);
 }
+
+
+
+// Write Your First String Variable
+
+let username = "per";
+// Create a variable, message, that stores the string: "You have tree new notifications"
+let message = "You have three new notifications"
+// Create a variable, messageToUser, that contains the message we have logged
+let messageToUser = message + ", " + username + "!";
+
+console.log(messageToUser);
