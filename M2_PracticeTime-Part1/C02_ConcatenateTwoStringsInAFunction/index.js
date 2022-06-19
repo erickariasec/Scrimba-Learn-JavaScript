f21259aaf54da0ddc0e2e94d7cd78e28b261f763
erickariasec <erickarias1999@hotmail.com>
2022-06-19 17:03:19 -0500

    Scrimba, Learn JavaScript, Practice Time - Part 1, C02 - Concatenate Two Strings in a Function

---
+++ M2_PracticeTime-Part1/C02_ConcatenateTwoStringsInAFunction/index.js
@@ -0,0 +1,10 @@
+let firstName = "Linda";
+let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+function greetingToLinda() {
+    let nameAndGreeting = greeting + ", " + firstName + "!";
+    console.log(nameAndGreeting);
+}
+
+greetingToLinda();
